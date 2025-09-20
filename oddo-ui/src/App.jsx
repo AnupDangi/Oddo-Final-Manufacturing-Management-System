03ab24bfb3d17a00235d522a3d687edb58239eb6
sameer-18sp <pandeyasameer4@gmail.com>
2025-09-20 15:05:15 +0530

    added Manufacturing_Order and Work_Order Components

--- oddo-ui/src/App.jsx
+++ oddo-ui/src/App.jsx
@@ -4,11 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import LoginComponent from './LoginComponent'
 import DashboardComponent from './DashboardComponent'
+import ManufacturingOrdersComponent from './ManufacturingOrdersComponent'
+import WorkOrdersComponent from './WorkOrdersComponent'
 
 function App() {
   return (
     <>
+      <LoginComponent />
       <DashboardComponent />
+      <ManufacturingOrdersComponent />
+      <WorkOrdersComponent />
     </>
   );
 }
